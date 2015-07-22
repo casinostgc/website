a63416819351e70486fbf832d7e5b2a0a9876e69
Kevin McCarthy <kjmagic13@gmail.com>
2015-07-22 10:53:47 -0400

    init image versions

--- app/models/concerns/s3_uploader.rb
+++ app/models/concerns/s3_uploader.rb
@@ -5,11 +5,24 @@ module S3Uploader
 		mount_uploader :image, ImageUploader
 
 		# attr_accessor :tmp_file, :tmp_s3_file
-
 		# after_create :upload_image, unless: -> { self.tmp_s3_file }
 	end
 
 	module ClassMethods
+
+		def reprocess(uploader=:image)
+			self.find_each do |model| 
+				begin
+					model.send(uploader).cache_stored_file! 
+					model.send(uploader).retrieve_from_cache!(model.send(uploader).cache_name) 
+					model.send(uploader).recreate_versions! 
+					model.save!
+				rescue => e
+					puts  "ERROR: #{self.to_s.titleize}: #{model.id} -> #{e.to_s}"
+				end
+			end
+		end
+
 	end
 
 	# def upload_image
